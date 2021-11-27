d7bc2a5255fb465f8ef50bdcf274fddc37255915
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-11-27 10:09:09 +0100

    Bumped coverage to v6.2 and transformed Common module to a package.

--- setup.py
+++ setup.py
@@ -31,87 +31,21 @@
 # SPDX-License-Identifier: Apache-2.0
 # ============================================================================
 #
-from ast        import parse as ast_parse, iter_child_nodes, Assign, Constant, Name
 from pathlib    import Path
-from setuptools import (
-	setup as setuptools_setup,
-	find_namespace_packages as setuptools_find_namespace_packages
-)
+from pyTooling.Packaging import DescribePythonPackageHostedOnGitHub
 
-gitHubNamespace =       "pyTooling"
-projectName =           "pyTooling"
-projectNameWithPrefix = f"{projectName}"
-__author =              None
-__email =               None
-__version =             None
+gitHubNamespace =        "pyTooling"
+packageName =            "pyTooling"
+packageDirectory =       packageName
+packageInformationFile = Path(f"{packageDirectory}/__init__.py")
+#pythonVersions =         ["3.8", "3.9", "3.10"]
 
-# Read __version__ from source file
-versionFile = Path(f"{projectName}/Common.py")
-with versionFile.open("r") as file:
-	for item in iter_child_nodes(ast_parse(file.read())):
-		if isinstance(item, Assign) and len(item.targets) == 1:
-			target = item.targets[0]
-			value =  item.value
-			if isinstance(target, Name) and target.id == "__author__" and isinstance(value, Constant) and isinstance(value.value, str):
-				__author = value.value
-			if isinstance(target, Name) and target.id == "__email__" and isinstance(value, Constant) and isinstance(value.value, str):
-				__email = value.value
-			if isinstance(target, Name) and target.id == "__version__" and isinstance(value, Constant) and isinstance(value.value, str):
-				__version = value.value
-if __version is None:
-	raise AssertionError(f"Could not extract '__version__' from '{versionFile}'.")
-
-# Read README for upload to PyPI
-readmeFile = Path("README.md")
-with readmeFile.open("r") as file:
-	long_description = file.read()
-
-# Read requirements file and add them to package dependency list
-requirementsFile = Path("requirements.txt")
-with requirementsFile.open("r") as file:
-	requirements = [line for line in file.readlines()]
-
-# Derive URLs
-sourceCodeURL =     f"https://GitHub.com/{gitHubNamespace}/{projectNameWithPrefix}"
-documentationURL =  f"https://{gitHubNamespace}.GitHub.io/{projectNameWithPrefix}"
-
-# Assemble all package information
-setuptools_setup(
-	name=projectNameWithPrefix,
-	version=__version,
-	author=__author,
-	author_email=__email,
-	# maintainer="Patrick Lehmann",
-	# maintainer_email="Paebbels@gmail.com",
-	license='Apache 2.0',
-
+DescribePythonPackageHostedOnGitHub(
+	packageName=packageName,
 	description="pyTooling is a powerful collection of arbitrary useful classes, decorators, meta-classes and exceptions.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-
-	url=sourceCodeURL,
-	project_urls={
-		'Documentation': f"{documentationURL}",
-		'Source Code':   f"{sourceCodeURL}",
-		'Issue Tracker': f"{sourceCodeURL}/issues"
-	},
-
-	packages=setuptools_find_namespace_packages(exclude=["doc", "doc.*", "tests", "tests.*",]),
-	classifiers=[
-		"License :: OSI Approved :: Apache Software License",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python :: 3 :: Only",
-		"Programming Language :: Python :: 3.6",
-		"Programming Language :: Python :: 3.7",
-		"Programming Language :: Python :: 3.8",
-		"Programming Language :: Python :: 3.9",
-		"Programming Language :: Python :: 3.10",
-		"Development Status :: 5 - Production/Stable",
-		"Intended Audience :: Developers",
-		"Topic :: Utilities"
-	],
+	gitHubNamespace=gitHubNamespace,
 	keywords="Python3 Exceptions Decorators MetaClasses Versioning Collection",
-
-	python_requires='>=3.6',
-	install_requires=requirements,
+	sourceFileWithVersion=packageInformationFile,
+#	developmentStatus="stable",
+#	pythonVersions=pythonVersions
 )
