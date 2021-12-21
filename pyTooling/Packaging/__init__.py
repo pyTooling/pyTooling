141527bbb53c3f3e3c90fdfa7f98bc509b17bc42
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-12-21 18:19:56 +0100

    Fixed typo in pyTooling.Packaging.

--- pyTooling/Packaging/__init__.py
+++ pyTooling/Packaging/__init__.py
@@ -1,33 +1,32 @@
-# =============================================================================
-#             _____           _ _               ____            _               _
-#  _ __  _   |_   _|__   ___ | (_)_ __   __ _  |  _ \ __ _  ___| | ____ _  __ _(_)_ __   __ _
-# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` | | |_) / _` |/ __| |/ / _` |/ _` | | '_ \ / _` |
-# | |_) | |_| || | (_) | (_) | | | | | | (_| |_|  __/ (_| | (__|   < (_| | (_| | | | | | (_| |
-# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, (_)_|   \__,_|\___|_|\_\__,_|\__, |_|_| |_|\__, |
-# |_|    |___/                          |___/                             |___/         |___/
-# =============================================================================
-# Authors:            Patrick Lehmann
-#
-# Python package:     A set of helper functions to describe a Python package for setuptools.
-#
-# License:
-# ============================================================================
-# Copyright 2021-2021 Patrick Lehmann - Bötzingen, Germany
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#		http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-# ============================================================================
+# ==================================================================================================================== #
+#             _____           _ _               ____            _               _                                      #
+#  _ __  _   |_   _|__   ___ | (_)_ __   __ _  |  _ \ __ _  ___| | ____ _  __ _(_)_ __   __ _                          #
+# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` | | |_) / _` |/ __| |/ / _` |/ _` | | '_ \ / _` |                         #
+# | |_) | |_| || | (_) | (_) | | | | | | (_| |_|  __/ (_| | (__|   < (_| | (_| | | | | | (_| |                         #
+# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, (_)_|   \__,_|\___|_|\_\__,_|\__, |_|_| |_|\__, |                         #
+# |_|    |___/                          |___/                             |___/         |___/                          #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2021-2021 Patrick Lehmann - Bötzingen, Germany                                                             #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#		http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
 #
 """\
 A set of helper functions to describe a Python package for setuptools.
@@ -135,7 +134,7 @@ class VersionInformation:
 	_copyright:   str
 	_email:       str
 	_keywords:    List[str]
-	_licenses:    str
+	_license:     str
 	_description: str
 	_version:     str
 
@@ -157,6 +156,10 @@ class VersionInformation:
 		return self._copyright
 
 	@property
+	def Description(self) -> str:
+		return self._description
+
+	@property
 	def Email(self) -> str:
 		return self._email
 
@@ -165,7 +168,7 @@ class VersionInformation:
 		return self._keywords
 
 	@property
-	def Licenses(self) -> str:
+	def License(self) -> str:
 		return self._license
 
 	@property
