093feab3028e1b32d85cca9ed31a71a432e2ee4a
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-12-26 11:33:18 +0100

    Updated headers for Exceptions sub-package.

--- pyTooling/Exceptions/__init__.py
+++ pyTooling/Exceptions/__init__.py
@@ -1,38 +1,34 @@
-# =============================================================================
-#             _____           _ _
-#  _ __  _   |_   _|__   ___ | (_)_ __   __ _
-# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` |
-# | |_) | |_| || | (_) | (_) | | | | | | (_| |
-# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, |
-# |_|    |___/                          |___/
-# ==============================================================================
-# Authors:            Patrick Lehmann
+# ==================================================================================================================== #
+#                                                                                                                      #
+#             _____           _ _               _____                    _   _                                         #
+#  _ __  _   |_   _|__   ___ | (_)_ __   __ _  | ____|_  _____ ___ _ __ | |_(_) ___  _ __  ___                         #
+# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` | |  _| \ \/ / __/ _ \ '_ \| __| |/ _ \| '_ \/ __|                        #
+# | |_) | |_| || | (_) | (_) | | | | | | (_| |_| |___ >  < (_|  __/ |_) | |_| | (_) | | | \__ \                        #
+# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, (_)_____/_/\_\___\___| .__/ \__|_|\___/|_| |_|___/                        #
+# |_|    |___/                          |___/                     |_|                                                  #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2017-2021 Patrick Lehmann - Bötzingen, Germany                                                             #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
 #
-# Python package:     This package contains exception base classes and common exceptions.
-#
-# License:
-# ==============================================================================
-# Copyright 2017-2021 Patrick Lehmann - Bötzingen, Germany
-# Copyright 2007-2016 Technische Universität Dresden - Germany
-#                     Chair of VLSI-Design, Diagnostics and Architecture
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-# ==============================================================================
-#
-#from SphinxExtensions import DocumentMemberAttribute
-
 from ..Decorators import export
 
 
