60e56d51041fe3ff40cb83c732f26fde9d853a75
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-12-26 11:53:29 +0100

    Updated headers for Versioning sub-package.

--- pyTooling/Versioning/__init__.py
+++ pyTooling/Versioning/__init__.py
@@ -1,34 +1,36 @@
-# =============================================================================
-#             _____           _ _
-#  _ __  _   |_   _|__   ___ | (_)_ __   __ _
-# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` |
-# | |_) | |_| || | (_) | (_) | | | | | | (_| |
-# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, |
-# |_|    |___/                          |___/
-# =============================================================================
-# Authors:            Patrick Lehmann
+# ==================================================================================================================== #
+#             _____           _ _           __     __            _             _                                       #
+#  _ __  _   |_   _|__   ___ | (_)_ __   __ \ \   / /__ _ __ ___(_) ___  _ __ (_)_ __   __ _                           #
+# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` \ \ / / _ \ '__/ __| |/ _ \| '_ \| | '_ \ / _` |                          #
+# | |_) | |_| || | (_) | (_) | | | | | | (_| |\ V /  __/ |  \__ \ | (_) | | | | | | | | (_| |                          #
+# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, (_)_/ \___|_|  |___/_|\___/|_| |_|_|_| |_|\__, |                          #
+# |_|    |___/                          |___/                                          |___/                           #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2020-2021 Patrick Lehmann - Bötzingen, Germany                                                             #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
 #
-# Python package:     A common version class.
-#
-# License:
-# ============================================================================
-# Copyright 2020-2021 Patrick Lehmann - Bötzingen, Germany
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-# ============================================================================
-#
+"""\
+Implementation of semantic and date versioning version-numbers.
+"""
 from enum          import IntEnum
 from typing import Optional as Nullable, Any
 
