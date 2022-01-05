d96e80e1f1a24b9796f8060d766ea743f9642f24
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-01-05 22:49:51 +0100

    Added configuration interface description.

---
+++ pyTooling/Configuration/__init__.py
@@ -0,0 +1,58 @@
+from typing import TypeVar, Union, ClassVar
+
+from pyTooling.Decorators import export
+
+KeyT = TypeVar("KeyT", bound=Union[str, int])
+NodeT = Union["Dictionary", "Sequence"]
+ValueT = TypeVar("ValueT", bound=Union[NodeT, str, int, float])
+
+
+@export
+class Node:
+	DICT_TYPE: ClassVar["Dictionary"]
+	SEQ_TYPE: ClassVar["Sequence"]
+	_parent: "Dictionary"
+	_root: "Configuration"
+
+	def __init__(self, root: "Configuration" = None, parent: NodeT = None):
+		self._root = root
+		self._parent = parent
+
+	def __getitem__(self, key: KeyT) -> ValueT:
+		raise NotImplementedError()
+
+	def __setitem__(self, key: KeyT, value: ValueT) -> None:
+		raise NotImplementedError()
+
+	def QueryPath(self, query: str) -> ValueT:
+		raise NotImplementedError()
+
+
+@export
+class Dictionary(Node):
+	pass
+
+
+@export
+class Sequence(Node):
+	def __getitem__(self, index: int) -> ValueT:
+		raise NotImplementedError()
+
+	def __setitem__(self, index: int, value: ValueT) -> None:
+		raise NotImplementedError()
+
+	def __iter__(self):
+		raise NotImplementedError()
+
+	def __next__(self):
+		raise NotImplementedError()
+
+
+setattr(Node, "DICT_TYPE", Dictionary)
+setattr(Node, "SEQ_TYPE", Sequence)
+
+
+@export
+class Configuration(Node):
+	def __init__(self):
+		Node.__init__(self)
