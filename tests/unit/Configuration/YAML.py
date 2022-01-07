5a9bf8d91393b728af3db11e9994be85a3187884
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-01-07 01:18:21 +0100

    Adjusted test cases.

--- tests/unit/Configuration/YAML.py
+++ tests/unit/Configuration/YAML.py
@@ -68,3 +68,11 @@ class ReadingValues(TestCase):
 
 		self.assertEqual(r"C:\VendorA\ToolA\2020", config["Install"]["VendorA"]["ToolA"][2020]["InstallDir"])
 		self.assertEqual(r"C:\VendorA\Tool_A\2021", config["Install"]["VendorA"]["ToolA"][2021]["InstallDir"])
+
+		self.assertEqual(r"C:\VendorA\ToolA\2020\bin", config["Install"]["VendorA"]["ToolA"][2020]["BinaryDir"])
+
+	def test_NestedVariables(self):
+		config = Configuration(Path("tests/unit/Configuration/config.yml"))
+
+		self.assertEqual(r"C:\VendorA\ToolA\2020", config["Install"]["VendorA"]["ToolA"]["Defaults"]["InstallDir"])
+		self.assertEqual(r"C:\VendorA\ToolA\2020\bin", config["Install"]["VendorA"]["ToolA"]["Defaults"]["BinaryDir"])
