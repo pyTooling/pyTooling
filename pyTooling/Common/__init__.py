06fc1d7e0a73656ea6091737de86533198c6cb61
Patrick Lehmann <Paebbels@gmail.com>
2023-03-26 15:05:23 +0200

    Bumped version to v4.1.0. [skip ci]

--- pyTooling/Common/__init__.py
+++ pyTooling/Common/__init__.py
@@ -37,7 +37,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2017-2022, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "4.0.1"
+__version__ =   "4.1.0"
 __keywords__ =  ["decorators", "meta-classes", "exceptions", "platform", "versioning", "licensing", "overloading",
 								"singleton", "tree", "graph", "timer", "data structure", "setuptools", "wheel", "installation",
 								"packaging", "path", "generic path", "generic library", "url", "terminal", "shell", "TUI", "console",
