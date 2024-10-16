f47dd6a7c1d2438e6b0022919be468231308e5dc
Patrick Lehmann <Paebbels@gmail.com>
2024-10-17 00:12:33 +0200

    Updated and enhanced Timer.

--- pyTooling/Timer/__init__.py
+++ pyTooling/Timer/__init__.py
@@ -33,19 +33,25 @@ A timer and stopwatch to measure execution time.
 
 .. hint:: See :ref:`high-level help <TIMER>` for explanations and usage examples.
 """
-from time   import perf_counter_ns
-from typing import List, Optional as Nullable, Dict
+from datetime import datetime
+from time     import perf_counter_ns
+from typing   import List, Optional as Nullable, Iterator, Tuple
+
 # Python 3.11: use Self if returning the own object: , Self
 
 try:
 	from pyTooling.Decorators  import export, readonly
 	from pyTooling.MetaClasses import SlottedObject
+	from pyTooling.Exceptions  import ToolingException
+	from pyTooling.Platform    import CurrentPlatform
 except (ImportError, ModuleNotFoundError):  # pragma: no cover
 	print("[pyTooling.Timer] Could not import from 'pyTooling.*'!")
 
 	try:
 		from Decorators          import export, readonly
 		from MetaClasses         import SlottedObject
+		from Exceptions          import ToolingException
+		from Platform            import CurrentPlatform
 	except (ImportError, ModuleNotFoundError) as ex:  # pragma: no cover
 		print("[pyTooling.Timer] Could not import directly!")
 		raise ex
@@ -59,64 +65,157 @@ class Timer(SlottedObject):
 	.. todo::TIMER::Timer Needs class documentation.
 	"""
 
-	_timers: Dict[str, 'Timer']
+	_name:         Nullable[str]
+	_beginTime:    Nullable[datetime]
+	_endTime:      Nullable[datetime]
+	_startTime:    Nullable[int]
+	_resumeTime:   Nullable[int]
+	_pauseTime:    Nullable[int]
+	_stopTime:     Nullable[int]
+	_totalTime:    Nullable[int]
+	_splits:       List[Tuple[float, bool]]
 
-	_startTime: Nullable[int]
-	_resumeTime: Nullable[int]
-	_pauseTime: int
-	_stopTime: int
-	_diffTime: int
-	_diffTimes: List[int]
+	def __init__(self, name: str = None) -> None:
+		self._name =         name
+		self._beginTime =    None
+		self._endTime =      None
+		self._startTime =    None
+		self._resumeTime =   None
+		self._pauseTime =    None
+		self._stopTime =     None
+		self._totalTime =    None
+		self._splits =       []
 
-	def __init__(self) -> None:
-		self._timers = {}
-
-		self._startTime = None
-		self._resumeTime = None
-		self._diffTimes = []
-
-	def __enter__(self):  # Python 3.11: -> Self:
+	def __enter__(self) -> "Timer":  # TODO: Python 3.11: -> Self:
 		self.Start()
 		return self
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
+	def __exit__(self, exc_type, exc_val, exc_tb) -> None:
 		self.Stop()
 
-	def Start(self):
+	def Start(self) -> None:
+		if self._startTime is not None:
+			raise ToolingException(f"Timer was already started.")
+
+		self._beginTime = datetime.now()
 		self._resumeTime = self._startTime = perf_counter_ns()
 
-	def Stop(self):
-		if self._startTime is None:
-			raise Exception(f"Timer was never started.")
+	def Split(self) -> float:
+		pauseTime = perf_counter_ns()
 
-		self._stopTime = perf_counter_ns()
-		self._diffTime = self._stopTime - self._startTime
+		if self._resumeTime is None:
+			raise ToolingException(f"Timer was not started or resumed.")
 
-		return self._diffTime / 1e9
+		diff = (pauseTime - self._resumeTime) / 1e9
+		self._splits.append((diff, True))
+		self._resumeTime = pauseTime
 
-	def Pause(self):
+		return diff
+
+	def Pause(self) -> float:
 		self._pauseTime = perf_counter_ns()
 
 		if self._resumeTime is None:
-			raise Exception(f"Timer was not (re-)started.")
+			raise ToolingException(f"Timer was not started or resumed.")
 
-		diff = self._pauseTime - self._resumeTime
-		self._diffTimes.append(diff)
+		diff = (self._pauseTime - self._resumeTime) / 1e9
+		self._splits.append((diff, True))
 		self._resumeTime = None
 
-		return diff / 1e9
+		return diff
 
-	def Continue(self):
+	def Resume(self) -> float:
 		self._resumeTime = perf_counter_ns()
+
+		if self._pauseTime is None:
+			raise ToolingException(f"Timer was not paused.")
+
+		diff = (self._resumeTime - self._pauseTime) / 1e9
+		self._splits.append((diff, False))
+		self._pauseTime = None
+
+		return diff
+
+	def Stop(self):
+		self._stopTime = perf_counter_ns()
+		self._endTime = datetime.now()
+
+		if self._startTime is None:
+			raise ToolingException(f"Timer was never started.")
+		if self._totalTime is not None:
+			raise ToolingException(f"Timer was already stopped.")
+
+		# Check is timer is currently paused
+		if self._resumeTime is None:
+			diff = (self._stopTime - self._pauseTime) / 1e9
+			self._splits.append((diff, False))
+		else:
+			diff = (self._stopTime - self._resumeTime) / 1e9
+			self._splits.append((diff, True))
+
+		self._totalTime = self._stopTime - self._startTime
+
+		beginEndDiff = self._endTime - self._beginTime
+
+		INACCURACY = 0.09 if CurrentPlatform.IsNativeMacOS else 0.02
+		assert abs(((sum(d for d, _ in self._splits)) / beginEndDiff.total_seconds()) - 1.0) < INACCURACY
+		assert abs(((self._totalTime / 1e9) / beginEndDiff.total_seconds()) - 1.0) < INACCURACY
+
+		return diff
+
+	@readonly
+	def Name(self) -> Nullable[str]:
+		return self._name
+
+	@readonly
+	def StartTime(self) -> Nullable[datetime]:
+		return self._beginTime
+
+	@readonly
+	def StopTime(self) -> Nullable[datetime]:
+		return self._endTime
+
+	@readonly
+	def HasSplitTimes(self) -> bool:
+		return len(self._splits) > 1
+
+	@readonly
+	def SplitCount(self) -> int:
+		return len(self._splits) - 1
+
+	@readonly
+	def ActiveCount(self) -> int:
+		return len(list(t for t, a in self._splits if a is True))
+
+	@readonly
+	def InactiveCount(self) -> int:
+		return len(list(t for t, a in self._splits if a is False))
+
+	@readonly
+	def Activity(self) -> float:
+		return sum(t for t, a in self._splits if a is True)
+
+	@readonly
+	def Inactivity(self) -> float:
+		return sum(t for t, a in self._splits if a is False)
 
 	@readonly
 	def Duration(self) -> float:
-		return self._diffTime / 1e9
+		return ((perf_counter_ns() - self._startTime) if self._stopTime is None else self._totalTime) / 1e9
 
 	@readonly
 	def DurationMS(self) -> float:
-		return self._diffTime / 1e6
+		return ((perf_counter_ns() - self._startTime) if self._stopTime is None else self._totalTime) / 1e6
 
 	@readonly
 	def DurationUS(self) -> float:
-		return self._diffTime / 1e3
+		return ((perf_counter_ns() - self._startTime) if self._stopTime is None else self._totalTime) / 1e3
+
+	def __len__(self):
+		return len(self._splits) - 1
+
+	def __getitem__(self, index: int) -> Tuple[float, bool]:
+		return self._splits[index]
+
+	def __iter__(self) -> Iterator[Tuple[float, bool]]:
+		return self._splits.__iter__()
--- tests/unit/Common/Timer.py
+++ tests/unit/Common/Timer.py
@@ -32,6 +32,8 @@
 from time               import sleep
 from unittest           import TestCase
 
+from pyTooling.Exceptions import ToolingException
+
 from pyTooling.Platform import CurrentPlatform
 from pyTooling.Timer    import Timer
 
@@ -43,49 +45,124 @@ if __name__ == "__main__":  # pragma: no cover
 
 
 class Timing(TestCase):
+	DELAY = 0.5
+	PAUSE = 0.9
+	INACCURACY = 2.7 if CurrentPlatform.IsNativeMacOS else 1.25
+
+	def test_StartStart(self) -> None:
+		timer = Timer()
+		timer.Start()
+		with self.assertRaises(ToolingException):
+			timer.Start()
+
+	def test_Split(self) -> None:
+		timer = Timer()
+		with self.assertRaises(ToolingException):
+			timer.Split()
+
+	def test_Pause(self) -> None:
+		timer = Timer()
+		with self.assertRaises(ToolingException):
+			timer.Pause()
+
+	def test_Resume(self) -> None:
+		timer = Timer()
+		with self.assertRaises(ToolingException):
+			timer.Resume()
+
+	def test_Stop(self) -> None:
+		timer = Timer()
+		with self.assertRaises(ToolingException):
+			timer.Stop()
+
 	def test_StartStop(self) -> None:
-		delay = 0.1
-		inaccuracy = 3.0 if CurrentPlatform.IsNativeMacOS else 0.5
+		print()
 
 		timer = Timer()
 
 		timer.Start()
-		sleep(delay)  # 100 ms
+		sleep(self.DELAY)  # 100 ms
 		diff = timer.Stop()
 
-		print(f"Duration for 'sleep({delay:0.3f})': {diff:0.6f} us")
-		self.assertLessEqual(diff, delay + (delay * inaccuracy))
+		print(f"Duration for 'sleep({self.DELAY:0.3f})': {diff:0.6f} us")
+		self.assertLessEqual(diff, self.DELAY * self.INACCURACY)
 
-	def test_PauseResume(self) -> None:
-		delay = 0.1
-		inaccuracy = 4.0 if CurrentPlatform.IsNativeMacOS else 0.5
+		self.assertFalse(timer.HasSplitTimes)
+		self.assertEqual(0, timer.SplitCount)
+		self.assertEqual(1, timer.ActiveCount)
+		self.assertEqual(0, timer.InactiveCount)
+		self.assertEqual(0, len(timer))
+
+	def test_StartPauseStop(self) -> None:
+		print()
 
 		timer = Timer()
 
 		timer.Start()
-		sleep(delay)  # 100 ms
-		diff = timer.Pause()
-		print(f"Duration for '1st sleep({delay:0.3f})': {diff:0.6f} us")
-		self.assertLessEqual(diff, delay + (delay * inaccuracy))
+		sleep(self.DELAY)  # 100 ms
+		diff1 = timer.Pause()
+		sleep(self.PAUSE)  # 200 ms
+		diff2 = timer.Stop()
+		total = timer.Duration
 
-		sleep(delay * 5)  # 500 ms
+		print(f"Duration for '1st sleep({self.DELAY:0.3f})': {diff1:0.6f} us")
+		self.assertLessEqual(diff1, self.DELAY * self.INACCURACY)
 
-		timer.Continue()
-		sleep(delay)  # 100 ms
-		diff = timer.Pause()
-		print(f"Duration for '2nd sleep({delay:0.3f})': {diff:0.6f} us")
-		self.assertLessEqual(diff, delay + (delay * inaccuracy))
+		print(f"Duration for '1st pause({self.PAUSE * 5:0.3f})': {diff2:0.6f} us")
+		self.assertLessEqual(diff2, self.PAUSE * self.INACCURACY)
 
-		total = timer.Stop()
-		print(f"Duration for '2x sleep({delay:0.3f}) + 1x pause({delay * 5:0.3f})': {total:0.6f} us")
-		self.assertLessEqual(total, (7 * delay) + (delay * inaccuracy))
+		print(f"Duration for '1x sleep({self.DELAY:0.3f}) + 1x pause({self.PAUSE:0.3f})': {total:0.6f} us")
+		self.assertLessEqual(total, (1 * self.DELAY + 1 * self.PAUSE) * self.INACCURACY)
+
+		self.assertTrue(timer.HasSplitTimes)
+		self.assertEqual(1, timer.SplitCount)
+		self.assertEqual(1, timer.ActiveCount)
+		self.assertEqual(1, timer.InactiveCount)
+		self.assertEqual(1, len(timer))
+
+	def test_StartPauseResumeStop(self) -> None:
+		print()
+
+		timer = Timer()
+
+		timer.Start()
+		sleep(self.DELAY)  # 100 ms
+		diff1 = timer.Pause()
+		sleep(self.PAUSE)  # 200 ms
+		diff2 = timer.Resume()
+		sleep(self.DELAY)  # 100 ms
+		diff3 = timer.Stop()
+		total = timer.Duration
+
+		print(f"Duration for '1st sleep({self.DELAY:0.3f})': {diff1:0.6f} us")
+		self.assertLessEqual(diff1, self.DELAY * self.INACCURACY)
+
+		print(f"Duration for '1st pause({self.PAUSE:0.3f})': {diff2:0.6f} us")
+		self.assertLessEqual(diff2, self.PAUSE * self.INACCURACY)
+
+		print(f"Duration for '2nd sleep({self.DELAY:0.3f})': {diff3:0.6f} us")
+		self.assertLessEqual(diff3, self.DELAY * self.INACCURACY)
+
+		print(f"Duration for '2x sleep({self.DELAY:0.3f}) + 1x pause({self.PAUSE:0.3f})': {total:0.6f} us")
+		self.assertLessEqual(total, (2 * self.DELAY + 1 * self.PAUSE) * self.INACCURACY)
+
+		seq = ((diff1, True), (diff2, False), (diff3, True))
+
+		self.assertTrue(timer.HasSplitTimes)
+		self.assertEqual(2, timer.SplitCount)
+		self.assertEqual(2, timer.ActiveCount)
+		self.assertEqual(1, timer.InactiveCount)
+		self.assertEqual(2, len(timer))
+		self.assertTupleEqual(seq[0], timer[0])
+		self.assertTupleEqual(seq[1], timer[1])
+		self.assertTupleEqual(seq[2], timer[2])
+		self.assertTupleEqual(seq, tuple(t for t in timer))
 
 	def test_ContextManager(self) -> None:
-		delay = 0.1
-		inaccuracy = 3.0 if CurrentPlatform.IsNativeMacOS else 0.5
+		print()
 
 		with Timer() as timer:
-			sleep(delay)  # 100 ms
+			sleep(self.DELAY)  # 100 ms
 
-		print(f"Duration for '2nd sleep({delay:0.3f})': {timer.Duration:0.6f} us")
-		self.assertLessEqual(timer.Duration, delay + (delay * inaccuracy))
+		print(f"Duration for '1st sleep({self.DELAY:0.3f})': {timer.Duration:0.6f} us")
+		self.assertLessEqual(timer.Duration, self.DELAY * self.INACCURACY)
