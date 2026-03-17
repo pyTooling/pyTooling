5d981784c56ed8d4cdb2da0127a8374b9ed6f7c0
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2026-03-17 18:14:28 +0100

    Bumped version to v8.13.0.

--- run.ps1
+++ run.ps1
@@ -34,7 +34,7 @@ Param(
 )
 
 $PackageName = "pyTooling"
-$PackageVersion = "8.12.0"
+$PackageVersion = "8.13.0"
 
 # set default values
 $EnableDebug =        [bool]$PSCmdlet.MyInvocation.BoundParameters["Debug"]
