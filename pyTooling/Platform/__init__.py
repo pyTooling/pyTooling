38eca40e3ae6b81c4e350936bf1000ea5c83ef9c
Yuri Victorovich <yuri@FreeBSD.org>
2026-09-19 14:25:43 -0700

    FreeBSD support

--- pyTooling/Platform/__init__.py
+++ pyTooling/Platform/__init__.py
@@ -306,6 +306,8 @@ class Platform(metaclass=ExtendedType, singleton=True, slots=True):
 			elif sys_platform.startswith("freebsd"):
 				if machine == "amd64":
 					self._platform = Platforms.FreeBSD
+				elif machine in ("aarch64", "arm64"):
+					self._platform = Platforms.OS_FreeBSD | Platforms.ENV_Native | Platforms.ARCH_AArch64
 				else:  # pragma: no cover
 					raise UnknownPlatformException(f"Unknown architecture '{machine}' for FreeBSD.")
 			else:  # pragma: no cover
@@ -366,11 +368,11 @@ class Platform(metaclass=ExtendedType, singleton=True, slots=True):
 
 	@readonly
 	def IsNativeFreeBSD(self) -> bool:
-		"""Returns true, if the platform is a :term:`native` FreeBSD x86-64 platform.
+		"""Returns true, if the platform is a :term:`native` FreeBSD platform.
 
-		:returns: ``True``, if the platform is a native FreeBSD x86-64 platform.
+		:returns: ``True``, if the platform is a native FreeBSD platform.
 		"""
-		return Platforms.FreeBSD in self._platform
+		return (Platforms.OS_FreeBSD in self._platform) and (Platforms.ENV_Native in self._platform)
 
 	@readonly
 	def IsNativeMacOS(self) -> bool:
